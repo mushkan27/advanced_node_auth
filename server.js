a64a5b2a852804d3116f9ac71af70f3ca149e3a3
mushkan27 <muskanyon@gmail.com>
2025-05-31 18:16:40 +0545

    feat(db): establish MongoDB connection and update server setup

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require('dotenv').config({path: "./config.env"})
 const express = require('express');
+const connectDB = require('./config/db')
+
+//Connect DB
+connectDB()
 
 const app = express()
 
@@ -9,4 +13,10 @@ app.use('/api/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+
+//Global eventlistener setup for unhandled promise rejectionmeaning when a Promise in your code rejects (fails) but you didn’t catch or handle that error anywhere.
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Logged Error: ${err}`)
+    server.close(()=>process.exit(1))
+})
